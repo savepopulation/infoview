43b069ddb802fc312244e221d4769b350199df4a
tyln <taylan.sbrcn@gmail.com>
2018-10-01 16:57:13 +0300

    info view added

---
+++ infoview/src/main/java/com/raqun/infoview/InfoView.kt
@@ -0,0 +1,12 @@
+package com.raqun.infoview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class InfoView constructor(context: Context,
+                           attrs: AttributeSet? = null,
+                           defStyle: Int = 0,
+                           defStyleRes: Int = 0) : LinearLayout(context, attrs) {
+    // emty info view
+}
