c594ba83e4a2472bba425d38a48c6f22e5900811
tyln <taylan.sbrcn@gmail.com>
2018-10-03 16:50:42 +0300

    info view init implemented

--- infoview/src/main/java/com/raqun/infoview/InfoView.kt
+++ infoview/src/main/java/com/raqun/infoview/InfoView.kt
@@ -6,6 +6,18 @@ import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.support.annotation.DrawableRes
 import android.support.annotation.ColorRes
+import com.raqun.infoview.InfoView.InfoType
+import android.view.ViewGroup
+import android.support.annotation.NonNull
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.LayoutInflater
+import com.raqun.infoview.InfoView.InfoType
+import android.content.res.TypedArray
+
+
+
+
 
 
 @SuppressLint("ViewConstructor")
@@ -13,7 +25,59 @@ class InfoView constructor(context: Context,
                            attrs: AttributeSet? = null,
                            defStyle: Int = 0,
                            defStyleRes: Int = 0) : LinearLayout(context, attrs) {
-    // emty info view
+    /**
+     * Vars
+     */
+
+    private var mInfoType = InfoType.INFO
+
+    @DrawableRes
+    private var mIconRes: Int = NO_RES
+
+    private var mInfoTitle: CharSequence? = null
+
+    private var mInfoMessage: CharSequence? = null
+
+    companion object {
+        private const val NO_RES = 0
+    }
+
+    /**
+     * Views
+     */
+
+    private val mImageViewIcon: ImageView
+
+    private val mTextViewTitle: TextView
+
+    private val mTextViewMessage: TextView
+
+    private val mContainer: ViewGroup
+
+    init {
+        val fields = context.theme
+                .obtainStyledAttributes(attrs, R.styleable.CustomInfoView, 0, 0)
+
+        try {
+            mInfoType = InfoType.fromValue(fields.getInt(R.styleable.CustomInfoView_infoType,
+                    InfoType.INFO.value))
+            mIconRes = fields.getInt(R.styleable.CustomInfoView_infoIcon, 0)
+            mInfoTitle = fields.getString(R.styleable.CustomInfoView_infoTitle)
+            mInfoMessage = fields.getString(R.styleable.CustomInfoView_infoText)
+        } finally {
+            fields.recycle()
+        }
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.custom_info_view, this, true)
+
+        mImageViewIcon = view.findViewById(R.id.icon)
+        mTextViewTitle = view.findViewById(R.id.title)
+        mTextViewMessage = view.findViewById(R.id.message)
+        mContainer = view.findViewById(R.id.container)
+
+        initView()
+    }
 
     // INFO TYPES
     enum class InfoType private constructor(private val value: Int,
